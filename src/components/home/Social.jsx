3a6a51c04eedf2e5bf64f96b31d28ce818d33030
necip <43618473+ncpy@users.noreply.github.com>
2023-04-04 22:42:38 +0300

    home-1

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://linkedin.com/in/necip-yolcu" className="home__social-icon" target='_blank'>
+                <i class="uil uil-linkedin"></i>
+            </a>
+            <a href="https://github.com/ncpy" className="home__social-icon" target='_blank'>
+                <i class="uil uil-github"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
