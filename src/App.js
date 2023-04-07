f1bdeac467a74f4bef156710c57d650ef7399fdc
necip <43618473+ncpy@users.noreply.github.com>
2023-04-07 12:23:07 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Services />
         <Qualification />
         <Contact />
+        <Footer />
       </main>
     </>
   );
