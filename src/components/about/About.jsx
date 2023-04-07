bf0b19dcebb0891fc68a3c4e9cbe9dc12da66dc5
necip <43618473+ncpy@users.noreply.github.com>
2023-04-08 02:11:10 +0300

    header nav link update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <span className="section__subtitle">My Introduction</span>
 
             <div className="about__container container grid">
-                <img src={ProfileImg} alt="" className="about__img" />
+                {/* <img src={ProfileImg} alt="" className="about__img" /> */}
 
                 <div className="about__data">
                     <Info />
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
                             <a
                                 href="#about"
                                 onClick={() => setActiveNav('#about')}
-                                className={activeNav === '#home'
+                                className={activeNav === '#about'
                                     ? "nav__link active-link"
                                     : "nav__link"}
                             >
@@ -48,7 +48,7 @@ const Header = () => {
                             <a
                                 href="#skills"
                                 onClick={() => setActiveNav('#skills')}
-                                className={activeNav === '#home'
+                                className={activeNav === '#skills'
                                     ? "nav__link active-link"
                                     : "nav__link"}
                             >
@@ -60,7 +60,7 @@ const Header = () => {
                             <a
                                 href="#services"
                                 onClick={() => setActiveNav('#services')}
-                                className={activeNav === '#home'
+                                className={activeNav === '#services'
                                     ? "nav__link active-link"
                                     : "nav__link"}
                             >
@@ -72,7 +72,7 @@ const Header = () => {
                             <a
                                 href="#portfolio"
                                 onClick={() => setActiveNav('#portfolio')}
-                                className={activeNav === '#home'
+                                className={activeNav === '#portfolio'
                                     ? "nav__link active-link"
                                     : "nav__link"}
                             >
@@ -84,7 +84,7 @@ const Header = () => {
                             <a
                                 href="#contact"
                                 onClick={() => setActiveNav('#contact')}
-                                className={activeNav === '#home'
+                                className={activeNav === '#contact'
                                     ? "nav__link active-link"
                                     : "nav__link"}
                             >
