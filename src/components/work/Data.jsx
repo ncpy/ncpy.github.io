c6535f15132079805cb83847ca04c3ac35575387
necip <43618473+ncpy@users.noreply.github.com>
2023-04-08 05:38:47 +0300

    work

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,45 @@
+import Work1 from '../../assets/work1.jpg'
+import Work2 from '../../assets/work2.jpg'
+import Work3 from '../../assets/work3.jpg'
+import Work4 from '../../assets/work4.jpg'
+import Work5 from '../../assets/work5.jpg'
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: "Catering",
+        category: 'Web',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Content Management",
+        category: 'Desktop',
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Camera Streaming",
+        category: 'App',
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "Content Management",
+        category: 'Web',
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: "Portfolio",
+        category: 'Web',
+    },
+]
+
+export const projectsNav = [
+    { name: 'All', },
+    { name: 'Web', },
+    { name: 'App', },
+    { name: 'Desktop', },
+]
